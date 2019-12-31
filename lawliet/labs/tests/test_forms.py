993acde96ade68f79c013bcc9d7f22481214d1d2
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2019-12-30 22:55:44 -0700

    Create a subdirectory in the 'labs' app to store tests. Add new tests for the LabEnvironment model and functional tests for the lab upload / edit pages.

--- lawliet/labs/tests/test_forms.py
+++ lawliet/labs/tests/test_forms.py
@@ -1,56 +1,12 @@
 import os
 
-from django.test import tag
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import tag
 
-from labs.models import LabEnvironment
 from labs.forms import LabUploadForm
-from lawliet.test_utils import UnitTest, random_docker_image
-
-BASE_DIR = settings.BASE_DIR
-
-STATIC_IMG_PATH = os.path.join(BASE_DIR, "assets", "img")
-STATIC_IMG_PATH = os.path.abspath(STATIC_IMG_PATH)
-
-"""
----------------------------------------------------
-LabEnvironment tests
----------------------------------------------------
-"""
-
-
-@tag("labs")
-class LabEnvironmentTestCase(UnitTest):
-    def setUp(self):
-        super().setUp()
-
-        with open(os.path.join(STATIC_IMG_PATH, "meepy.png"), "rb") as img:
-            self.cutter_image = SimpleUploadedFile(
-                name="cutter.png", content=img.read(), content_type="image/png"
-            )
-
-        labenv = LabEnvironment.objects.create(
-            name="Cutter + Radare2",
-            description="Cutter & Radare2 desktop",
-            url=random_docker_image(self.rd),
-            header_image=self.cutter_image,
-        )
-
-    def test_can_retrieve_basic_environment(self):
-        """
-        Retrieve the environment created in the setUp stage and test whether the
-        environment was constructed correctly.
-        """
-        cutter_lab = LabEnvironment.objects.get(name="Cutter + Radare2")
-        path = cutter_lab.header_image.path
-        self.assertEqual(cutter_lab.description, "Cutter & Radare2 desktop")
-        self.assertTrue(path.startswith(settings.MEDIA_ROOT))
-        self.assertTrue(
-            path.startswith(os.path.join(settings.MEDIA_ROOT, "labs", "img"))
-        )
-        self.assertTrue(cutter_lab.has_web_interface)  # True by default
-
+from labs.models import LabEnvironment
+from lawliet.test_utils import UnitTest
 
 """
 ---------------------------------------------------
@@ -61,6 +17,12 @@ LabUploadForm tests
 
 @tag("forms", "labs")
 class LabUploadFormTestCase(UnitTest):
+    """
+        class LabUploadFormTestCase(lawliet.test_utils.UnitTest)
+
+    Unit tests for labs.forms.LabUploadForm.
+    """
+
     def setUp(self):
         super().setUp(create_user=True)
         self.user.is_staff = True
---
+++ lawliet/labs/tests/test_interactive.py
@@ -0,0 +1,120 @@
+"""
+Functional tests for the labs app.
+"""
+
+import os
+
+from django.test import tag
+from django.urls import reverse
+
+from lawliet.test_utils import FunctionalTest
+from users.models import User
+
+
+@tag("labs")
+class UploadNewLabTestCase(FunctionalTest):
+    """
+    Functional tests for uploading new labs to the site.
+    """
+
+    def setUp(self):
+        super().setUp(preauth=True)
+        self.lab_name = "My Test Lab"
+        self.lab_description = "This is my test lab!"
+        self.lab_url = "https://hub.docker.com/wshand/cutter"
+        self.lab_header_image = os.path.abspath(
+            os.path.join("assets", "img", "meepy.png")
+        )
+        self.lab_has_web_interface = True
+
+    """
+    Helper functions
+    """
+
+    def get_lab_button(self):
+        sidebar = self.browser.find_element_by_id("sidebar-wrapper")
+        sidebar_buttons = sidebar.find_elements_by_class_name("sidebar-button")
+        buttons = list(filter(lambda btn: btn.text == "Labs", sidebar_buttons))
+        self.assertEqual(len(buttons), 1)
+        return buttons[0]
+
+    def get_lab_submenu(self):
+        return self.browser.find_element_by_id("lab-menu")
+
+    def get_lab_submenu_button(self, label):
+        menu = self.get_lab_submenu()
+        buttons = menu.find_elements_by_class_name("list-group-item")
+        buttons = list(filter(lambda btn: btn.text == label, buttons))
+        self.assertEqual(len(buttons), 1)
+        return buttons[0]
+
+    """
+    Tests
+    """
+
+    @tag("sidebar")
+    def test_nonstaff_cannot_access_lab_upload_page(self):
+        # Meepy wants to upload a new lab to the site. However, as a non-staff
+        # user, she is unable to do so.
+        # She clicks the "Labs" button on the sidebar. There isn't any button
+        # to upload a new lab to the site.
+        self.browser.get(self.live_server_url)
+        self.get_lab_button().click()
+
+        menu = self.get_lab_submenu()
+        menu_buttons = menu.find_elements_by_class_name("list-group-item")
+        for button in menu_buttons:
+            self.assertFalse("Upload".lower() in button.text.lower())
+
+        # As a staff user, Meepy sees this upload form.
+        user = User.objects.get(username=self.username)
+        user.is_staff = True
+        user.save()
+
+        self.browser.get(self.live_server_url)
+        self.get_lab_button().click()
+        self.get_lab_submenu_button("Upload new").click()
+
+        self.wait_for(lambda: self.assertIn("Upload lab", self.browser.title))
+
+    @tag("tmp")
+    def test_upload_new_lab(self):
+        user = User.objects.get(username=self.username)
+        user.is_staff = True
+        user.save()
+
+        # Meepy goes to the lab upload page, where she's presented with a form
+        # for adding a new LabEnvironment to the site.
+        self.browser.get(self.live_server_url + reverse("upload lab"))
+
+        self.wait_for(lambda: self.assertIn("Upload lab", self.browser.title))
+
+        namebox = self.browser.find_element_by_id("id_name")
+        descbox = self.browser.find_element_by_id("id_description")
+        urlbox = self.browser.find_element_by_id("id_url")
+        imgbox = self.browser.find_element_by_id("id_header_image")
+        webbox = self.browser.find_element_by_id("id_has_web_interface")
+
+        placeholders = {
+            namebox: "Environment name",
+            descbox: "Add a description of the lab environment",
+            urlbox: "ex. https://hub.docker.com/r/wshand/cutter",
+        }
+        for box in placeholders:
+            self.assertEqual(box.get_attribute("placeholder"), placeholders[box])
+
+        # Meepy fills out all of the forms on the page to add a new lab environment
+        namebox.send_keys(self.lab_name)
+        descbox.send_keys(self.lab_description)
+        urlbox.send_keys(self.lab_url)
+        imgbox.send_keys(self.lab_header_image)
+
+        # By default, the "has web interface" box should be selected.
+        self.assertTrue(webbox.is_selected)
+
+        # Meepy clicks the "save lab" button
+        save_button = self.browser.find_element_by_id("lab-save-button")
+        self.assertEqual(save_button.text, "Save lab")
+        save_button.click()
+
+        self.fail("TODO (test_upload_new_lab)")
